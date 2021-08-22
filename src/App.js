0d1696ca2e20dbb7416496c4d2a985bcb4ebb546
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-08-22 16:01:20 +0600

    Normal Stracture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hello', 'Hi', 'Whats up']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Programmers!!!</h1>
+    <form>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <Button variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+    </form>
+
+
+      {/* Messages themselves */}
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
